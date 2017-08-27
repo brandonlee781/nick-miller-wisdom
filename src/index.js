4657ae11b3632ddd551d9f1bed79f4415021648a
Brandon Lee <blee@Brandons-Mac-mini-2.local>
2017-08-27 17:50:39 -0500

    Basic working example

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const wisdom = require('./wisdom').wisdom;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  let quote = wisdom[Math.round(Math.random() * wisdom.length)];
+  res.send(quote)
+})
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+})
